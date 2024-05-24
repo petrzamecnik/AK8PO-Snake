86b69199d715e135a01e16ef0663d8a3cf4c638b
petrz <zamecnikpetr@protonmail.com>
2024-05-24 20:20:13 +0200

    final refactoring

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,20 +9,18 @@ namespace Snake
         public int ScreenHeight { get; set; } = 16;
         public int GameSpeed { get; set; } = 100;
         public int Score { get; set; } = 5;
-        public DateTime Time { get; set; } = DateTime.Now;
-        public DateTime Time2 { get; set; } = DateTime.Now;
 
         public ConsoleColor CookieColor { get; set; } = ConsoleColor.Red;
         public ConsoleColor BorderColor { get; set; } = ConsoleColor.Yellow;
-        public static Random RandomNumber { get; set; } = new Random();
+        public static Random RandomNumber { get; } = new Random();
     }
 
     public class Snake
     {
         public int HeadXPos { get; set; }
         public int HeadYPos { get; set; }
-        public List<int> BodyXPos { get; set; }
-        public List<int> BodyYPos { get; set; }
+        public List<int> BodyXPos { get; }
+        public List<int> BodyYPos { get; }
         public ConsoleColor SnakeHeadColor { get; set; }
         public ConsoleColor SnakeBodyColor { get; set; }
 
@@ -44,8 +42,8 @@ namespace Snake
 
     class Program
     {
-        private static int lastTailX = 0;
-        private static int lastTailY = 0;
+        private static int lastTailX;
+        private static int lastTailY;
 
         private static void Main(string[] args)
         {
@@ -56,7 +54,6 @@ namespace Snake
             var gameOver = false;
             var lastUpdateTime = DateTime.Now;
 
-
             var snake = new Snake
             {
                 HeadXPos = gameSettings.ScreenWidth / 2,
@@ -65,14 +62,14 @@ namespace Snake
                 SnakeBodyColor = ConsoleColor.Green,
             };
 
-
             DrawBorders(gameSettings);
+
             while (!gameOver)
             {
                 var currentTime = DateTime.Now;
                 var elapsedTime = (currentTime - lastUpdateTime).TotalMilliseconds;
-                if (!(elapsedTime >= gameSettings.GameSpeed)) continue;
-                
+                if (elapsedTime < gameSettings.GameSpeed) continue;
+
                 ProcessInput(ref movement);
                 UpdateSnakePosition(snake, movement, gameSettings);
                 gameOver = CheckGameOver(snake, gameSettings);
@@ -86,52 +83,26 @@ namespace Snake
                 lastUpdateTime = currentTime;
             }
 
-            GameOver(gameSettings);
+            DisplayGameOver(gameSettings);
         }
 
-        private static void DrawGameObject(int x, int y, ConsoleColor color, char symbol)
+        private static void DrawBorders(GameSettings gameSettings)
         {
-            Console.SetCursorPosition(x, y);
-            Console.ForegroundColor = color;
-            Console.Write(symbol);
+            Console.ForegroundColor = gameSettings.BorderColor;
+            DrawBorderLine(0, gameSettings.ScreenWidth, (i) => (i, 0));
+            DrawBorderLine(0, gameSettings.ScreenWidth, (i) => (i, gameSettings.ScreenHeight - 1));
+            DrawBorderLine(0, gameSettings.ScreenHeight, (i) => (0, i));
+            DrawBorderLine(0, gameSettings.ScreenHeight, (i) => (gameSettings.ScreenWidth - 1, i));
         }
 
-        private static bool CheckGameOver(Snake snake, GameSettings gameSettings)
+        private static void DrawBorderLine(int start, int end, Func<int, (int, int)> getCoordinates)
         {
-            // Check border collision
-            if (snake.HeadXPos < 1 || snake.HeadXPos >= gameSettings.ScreenWidth - 1 ||
-                snake.HeadYPos < 1 || snake.HeadYPos >= gameSettings.ScreenHeight - 1)
+            for (var i = start; i < end; i++)
             {
-                return true;
-            }
-
-            // Check if snake eats itself
-            for (var i = 0; i < snake.BodyXPos.Count; i++)
-            {
-                if (snake.BodyXPos[i] == snake.HeadXPos && snake.BodyYPos[i] == snake.HeadYPos)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-
-        private static void DrawSnake(Snake snake)
-        {
-            // Draw snake body
-            for (var i = 0; i < snake.BodyXPos.Count; i++)
-            {
-                Console.SetCursorPosition(snake.BodyXPos[i], snake.BodyYPos[i]);
-                Console.ForegroundColor = snake.SnakeBodyColor;
+                var (x, y) = getCoordinates(i);
+                Console.SetCursorPosition(x, y);
                 Console.Write("■");
             }
-
-            // Draw snake head
-            Console.SetCursorPosition(snake.HeadXPos, snake.HeadYPos);
-            Console.ForegroundColor = snake.SnakeHeadColor;
-            Console.Write("■");
         }
 
         private static void ProcessInput(ref MovementDirection movement)
@@ -145,6 +116,7 @@ namespace Snake
                 ConsoleKey.DownArrow when movement != MovementDirection.Up => MovementDirection.Down,
                 ConsoleKey.LeftArrow when movement != MovementDirection.Right => MovementDirection.Left,
                 ConsoleKey.RightArrow when movement != MovementDirection.Left => MovementDirection.Right,
+                _ => movement
             };
         }
 
@@ -171,42 +143,32 @@ namespace Snake
                     break;
             }
 
-            // Check if we should shorten the snake
-            if (snake.BodyXPos.Count <= gameSettings.Score) return;
-
-            lastTailX = snake.BodyXPos[0];
-            lastTailY = snake.BodyYPos[0];
-
-            // Remove the first element, which represents the tail segment
-            snake.BodyXPos.RemoveAt(0);
-            snake.BodyYPos.RemoveAt(0);
-        }
-
-        private static void GameOver(GameSettings gameSettings)
-        {
-            Console.SetCursorPosition(gameSettings.ScreenWidth / 5, gameSettings.ScreenHeight / 2);
-            Console.WriteLine("Game over, Score: " + gameSettings.Score);
-            Console.SetCursorPosition(gameSettings.ScreenWidth / 5, gameSettings.ScreenHeight / 2 + 1);
-            Console.ReadKey();
-        }
-
-        private static void DrawBorderLine(int start, int end, Func<int, (int, int)> getCoordinates)
-        {
-            for (var i = start; i < end; i++)
+            if (snake.BodyXPos.Count > gameSettings.Score)
             {
-                var (x, y) = getCoordinates(i);
-                Console.SetCursorPosition(x, y);
-                Console.Write("■");
+                lastTailX = snake.BodyXPos[0];
+                lastTailY = snake.BodyYPos[0];
+                snake.BodyXPos.RemoveAt(0);
+                snake.BodyYPos.RemoveAt(0);
             }
         }
 
-        private static void DrawBorders(GameSettings gameSettings)
+        private static bool CheckGameOver(Snake snake, GameSettings gameSettings)
         {
-            Console.ForegroundColor = gameSettings.BorderColor;
-            DrawBorderLine(0, gameSettings.ScreenWidth, (i) => (i, 0));
-            DrawBorderLine(0, gameSettings.ScreenWidth, (i) => (i, gameSettings.ScreenHeight - 1));
-            DrawBorderLine(0, gameSettings.ScreenHeight, (i) => (0, i));
-            DrawBorderLine(0, gameSettings.ScreenHeight, (i) => (gameSettings.ScreenWidth - 1, i));
+            if (snake.HeadXPos < 1 || snake.HeadXPos >= gameSettings.ScreenWidth - 1 ||
+                snake.HeadYPos < 1 || snake.HeadYPos >= gameSettings.ScreenHeight - 1)
+            {
+                return true;
+            }
+
+            for (var i = 0; i < snake.BodyXPos.Count; i++)
+            {
+                if (snake.BodyXPos[i] == snake.HeadXPos && snake.BodyYPos[i] == snake.HeadYPos)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private static void HandleCookieConsumption(ref int cookiePosX, ref int cookiePosY, Snake snake,
@@ -221,16 +183,43 @@ namespace Snake
 
         private static void RedrawGameWindow(Snake snake, int cookiePosX, int cookiePosY, GameSettings gameSettings)
         {
-            // Keep correct snake length
             if (snake.BodyXPos.Count >= gameSettings.Score)
             {
                 Console.SetCursorPosition(lastTailX, lastTailY);
-                Console.Write(" "); // Clear the last tail segment
+                Console.Write(" ");
             }
 
-            // Redraw the snake and the cookie
             DrawSnake(snake);
             DrawGameObject(cookiePosX, cookiePosY, gameSettings.CookieColor, '■');
+        }
+
+        private static void DrawSnake(Snake snake)
+        {
+            for (var i = 0; i < snake.BodyXPos.Count; i++)
+            {
+                Console.SetCursorPosition(snake.BodyXPos[i], snake.BodyYPos[i]);
+                Console.ForegroundColor = snake.SnakeBodyColor;
+                Console.Write("■");
+            }
+
+            Console.SetCursorPosition(snake.HeadXPos, snake.HeadYPos);
+            Console.ForegroundColor = snake.SnakeHeadColor;
+            Console.Write("■");
+        }
+
+        private static void DrawGameObject(int x, int y, ConsoleColor color, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(symbol);
+        }
+
+        private static void DisplayGameOver(GameSettings gameSettings)
+        {
+            Console.SetCursorPosition(gameSettings.ScreenWidth / 5, gameSettings.ScreenHeight / 2);
+            Console.WriteLine("Game over, Score: " + gameSettings.Score);
+            Console.SetCursorPosition(gameSettings.ScreenWidth / 5, gameSettings.ScreenHeight / 2 + 1);
+            Console.ReadKey();
         }
     }
 }
